57a28f945bac90580a6546998bd7ec0a10dc1b74
Dor <damrydor1@gmail.com>
2018-12-17 17:52:48 +0200

    company changes component

--- src/Components/CompanyChanges.js
+++ src/Components/CompanyChanges.js
@@ -7,7 +7,6 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
-import Utils from "./js/Utils";
 import classNames from "classnames";
 import ReactTooltip from "react-tooltip";
 import Dialog from "@material-ui/core/Dialog";
@@ -19,9 +18,48 @@ import {
   StyledCloseIcon,
   StyledDialogContent
 } from "./LowLevelComponents/StyledComponents";
+import Utils from "./js/Utils";
 import NoDataImg from "./LowLevelComponents/NoDataImg";
+import styled from "styled-components";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+const StyledExpansionPanel = styled(ExpansionPanel)`
+  padding-top: 0px;
+  padding-bottom: 0px;
+  text-align: left;
+  box-shadow: none !important;
+`;
+
+const StyledListItem = styled(ListItem)`
+  padding: 4px 0 4px 0 !important;
+`;
+
+const StyledListItemText = styled(ListItemText)`
+  margin-left: -25px;
+  padding-top: 2px !important;
+`;
+
+const StyledExpansionSummary = styled(ExpansionPanelSummary)`
+  min-height: 30px !important;
+  padding: 8px !important;
+  @media (max-width: 600px) {
+    padding: 0 !important;
+  }
+`;
+
+const StyledExpansionPanelDetails = styled(ExpansionPanelDetails)`
+  padding-top: 0px !important;
+  padding-bottom: 8px !important;
+  margin: 0px;
+`;
 
 const styles = {
+  expansionSummaryInner: {
+    margin: "0px !important"
+  },
   icon: {
     height: 24,
     width: 24
@@ -32,24 +70,9 @@ const styles = {
   },
   listItemSecondary: {
     textAlign: "left",
-    left: "65%",
+    left: "75%",
     transform: "none !important",
     top: "25%"
-  },
-  bottomMsg: {
-    marginTop: -10,
-    marginRight: 10,
-    marginLeft: 22,
-    textAlign: "left",
-    whiteSpace: "nowrap",
-    overflow: "hidden",
-    textOverflow: "ellipsis"
-  },
-  bottomIcon: {
-    position: "absolute",
-    marginTop: -4,
-    height: 24,
-    width: 24
   },
   topIcon: {
     marginTop: -2,
@@ -69,12 +92,12 @@ const styles = {
   }
 };
 
-class Certifications extends Component {
+class CompanyChanges extends Component {
   constructor(props) {
     super(props);
     this.state = {
       report: this.props.report,
-      allCertificatesOpen: false
+      allChangesOpen: false
     };
   }
 
@@ -88,9 +111,9 @@ class Certifications extends Component {
           otherWidth={"75%"}
         >
           <Typography className={classNames("fontStyle1")}>
-            Certifications
+            Changes in the Company
           </Typography>
-          <div data-tip data-for={"tipCertifications"}>
+          <div data-tip data-for={"tipChanges"}>
             <img
               alt="info"
               src={Utils.getIcon("info")}
@@ -99,17 +122,17 @@ class Certifications extends Component {
           </div>
           <ReactTooltip
             className={classNames("tooltip", "fontStyle14")}
-            id={"tipCertifications"}
+            id={"tipChanges"}
             place="right"
             effect="solid"
           >
-            <span>The supplier’s certifications</span>
+            <span>The supplier’s changes during the years</span>
           </ReactTooltip>
         </StyledTitle>
-        {this.state.report.certificates.length > 0 ? (
+        {this.state.report.changesList.length > 0 ? (
           <div
             className={classNames(classes.divViewAll, "fontStyle6")}
-            onClick={() => this.setState({ allCertificatesOpen: true })}
+            onClick={() => this.setState({ allChangesOpen: true })}
             data-cy="divViewAll"
           >
             View All
@@ -122,43 +145,47 @@ class Certifications extends Component {
         ) : (
           ""
         )}
-        {this.state.report.certificates.length > 0 ? (
+        {this.state.report.changesList.length > 0 ? (
           <div className={classes.listDiv}>
             <div className={classes.listDiv}>
               <List disablePadding={true} dense={true}>
-                {this.state.report.certificates
-                  .filter(certificate => certificate.name !== "")
+                {this.state.report.changesList
+                  .filter(change => change.changeItem !== "")
                   .slice(0, 5)
-                  .map((certificate, idx) => {
+                  .map((change, idx) => {
                     return (
                       <ListItem key={idx}>
                         <ListItemIcon className={classes.icon}>
                           <img
-                            alt="certificate"
-                            src={require("./images/Certification.svg")}
+                            alt="change"
+                            src={require("./images/change.svg")}
                           />
                         </ListItemIcon>
                         <ListItemText
-                          style={{ marginLeft: -25, maxWidth: "58%" }}
+                          style={{ marginLeft: -25, maxWidth: "68%" }}
                           primary={
                             <div>
-                              <div data-tip data-for={"tip" + certificate.name}>
+                              <div data-tip data-for={"tipchange" + idx}>
                                 <Typography
                                   className={classNames(
                                     "fontStyle7",
                                     classes.listItem
                                   )}
                                 >
-                                  {certificate.name}
+                                  {change.changeItem}
                                 </Typography>
                               </div>
                               <ReactTooltip
                                 className={classNames("tooltip", "fontStyle14")}
-                                id={"tip" + certificate.name}
+                                id={"tipchange" + idx}
                                 place="bottom"
                                 effect="solid"
                               >
-                                <span>{certificate.name}</span>
+                                <div>
+                                  <div>Change: {change.changeItem}</div>
+                                  <div>Before Change: {change.before}</div>
+                                  <div>After Change: {change.after}</div>
+                                </div>
                               </ReactTooltip>
                             </div>
                           }
@@ -169,9 +196,7 @@ class Certifications extends Component {
                             "fontStyle10"
                           )}
                         >
-                          {certificate.validity === 1
-                            ? "Valid (" + certificate.expiration + ")"
-                            : "Invalid"}
+                          {change.date}
                         </ListItemSecondaryAction>
                       </ListItem>
                     );
@@ -188,63 +213,71 @@ class Certifications extends Component {
               root: classes.dialog
             }
           }}
-          open={this.state.allCertificatesOpen}
-          onClose={() => this.setState({ allCertificatesOpen: false })}
+          open={this.state.allChangesOpen}
+          onClose={() => this.setState({ allChangesOpen: false })}
           aria-labelledby="scroll-dialog-title"
         >
           <StyledCloseIcon
-            onClick={() => this.setState({ allCertificatesOpen: false })}
+            onClick={() => this.setState({ allChangesOpen: false })}
           >
             <img alt="Close" src={require("./images/Close.png")} />
           </StyledCloseIcon>
           <DialogTitle className={"fontStyle3"} style={{ textAlign: "center" }}>
-            All Certificates
+            All Changes
           </DialogTitle>
           <StyledDialogContent>
             <List>
-              {this.state.report.certificates
-                .filter(certificate => certificate.name !== "")
-                .map((certificate, idx) => {
+              {this.state.report.changesList
+                .filter(change => change.changeItem !== "")
+                .map((change, idx) => {
                   return (
                     <div key={idx}>
-                      <ListItem
-                        style={{
-                          paddingTop: this.props.width > 600 ? "" : "16px",
-                          paddingBottom: this.props.width > 600 ? "" : "16px",
-                          paddingLeft: this.props.width > 600 ? "" : "8px"
-                        }}
-                      >
-                        <ListItemIcon className={classes.icon}>
-                          <img
-                            alt="certificate"
-                            src={require("./images/Certification.svg")}
-                          />
-                        </ListItemIcon>
-                        <ListItemText
-                          style={{ marginLeft: -25, maxWidth: "58%" }}
-                          primary={
-                            <Typography className={"fontStyle7"}>
-                              {certificate.name}
-                            </Typography>
-                          }
-                        />
-                        <ListItemSecondaryAction
-                          className={classNames(
-                            classes.listItemSecondary,
-                            "fontStyle10"
-                          )}
-                        >
-                          {certificate.validity === 1
-                            ? "Valid Until " + certificate.expiration
-                            : "Invalid"}
-                        </ListItemSecondaryAction>
-                      </ListItem>
-                      {this.state.report.certificates.indexOf(certificate) <
-                      this.state.report.certificates.length - 1 ? (
-                        <Divider />
-                      ) : (
-                        ""
-                      )}
+                      <StyledListItem>
+                        <StyledExpansionPanel style={{ width: "100%" }}>
+                          <StyledExpansionSummary
+                            IconButtonProps={{
+                              style: {
+                                padding: 0
+                              }
+                            }}
+                            classes={{
+                              content: classes.expansionSummaryInner,
+                              expanded: classes.expansionSummaryInner
+                            }}
+                            expandIcon={<ExpandMoreIcon />}
+                          >
+                            <ListItemIcon>
+                              <img
+                                height={24}
+                                width={24}
+                                alt={"change"}
+                                src={require("./images/change.svg")}
+                              />
+                            </ListItemIcon>
+                            <StyledListItemText
+                              primary={
+                                <Typography className={"fontStyle5"}>
+                                  {change.changeItem}
+                                </Typography>
+                              }
+                            />
+                          </StyledExpansionSummary>
+                          <StyledExpansionPanelDetails>
+                            <div>
+                              <Typography className={"fontStyle11"}>
+                                {"\u2022"} Change Date: {change.date}
+                              </Typography>
+                              <Typography className={"fontStyle11"}>
+                                {"\u2022"} Before Change: {change.before}
+                              </Typography>
+                              <Typography className={"fontStyle11"}>
+                                {"\u2022"} After Change: {change.after}
+                              </Typography>
+                            </div>
+                          </StyledExpansionPanelDetails>
+                        </StyledExpansionPanel>
+                      </StyledListItem>
+                      <Divider />
                     </div>
                   );
                 })}
@@ -256,8 +289,8 @@ class Certifications extends Component {
   }
 }
 
-Certifications.propTypes = {
+CompanyChanges.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(Certifications);
+export default withStyles(styles)(CompanyChanges);
